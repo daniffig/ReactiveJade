ca5755ee19ae31073bbe7f04daff784f57524dea
Lucio Di Giacomo Noack <daniffig@gmail.com>
2019-02-03 22:09:26 -0300

    added some hardware sniff methods

---
+++ reactivejade-desktop/hardwaresniffer/HardwareSniffer.java
@@ -0,0 +1,15 @@
+// package hardwaresniffer;
+
+// public class HardwareSniffer {
+
+//   public interface HardwareSniffer {
+
+//     public long getTotalPhysicalMemorySize();
+//     public long getFreePhysicalMemorySize();
+
+//   }
+
+//   public class Linux implements HardwareSniffer {
+
+//   }
+// }
--- reactivejade-desktop/hardwaresniffer/HardwareSnifferBehaviour.java
+++ reactivejade-desktop/hardwaresniffer/HardwareSnifferBehaviour.java
@@ -4,6 +4,11 @@ import jade.core.Agent;
 import jade.core.Location;
 import jade.core.behaviours.TickerBehaviour;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
 import java.util.Date;
 
 // http://jade.tilab.com/doc/api/jade/core/behaviours/TickerBehaviour.html
@@ -23,6 +28,23 @@ public class HardwareSnifferBehaviour extends TickerBehaviour {
     hsAgent.sendGenericMessage("I'm " + hsAgent.getName() + " and I'm on " + hsAgent.here().getName() + " at " + (new Date()));
     // hsAgent.notifyLocation();
 
+    this.sniffHardware();
+
     hsAgent.doMove(hsAgent.nextLocation());
+  }
+
+  private void sniffHardware() {
+    String meminfo = "/proc/meminfo";
+
+    try {
+      FileReader localFileReader = new FileReader(meminfo);
+      BufferedReader localBufferedReader = new BufferedReader(localFileReader, 8192);
+
+      hsAgent.sendGenericMessage(localBufferedReader.readLine());
+
+      localBufferedReader.close();
+    } catch (Exception e) {
+      hsAgent.sendGenericMessage(e.getMessage());
+    }
   }
 }
