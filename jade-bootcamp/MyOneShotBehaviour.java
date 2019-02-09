52c33ea0e53ac97ed9ee1a8b2d1fa298f7c39cc8
Lucio Di Giacomo Noack <daniffig@gmail.com>
2019-02-08 21:19:51 -0300

    cleaned project files

--- jade-bootcamp/MyOneShotBehaviour.java
+++
@@ -1,8 +0,0 @@
-import jade.core.behaviours.OneShotBehaviour;
-
-public class MyOneShotBehaviour extends OneShotBehaviour {
-
-  public void action() {
-    System.out.println("General Kenobi!");
-  }
-}
