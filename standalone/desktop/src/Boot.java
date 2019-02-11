40226a9104813a507243047f08fb092fc35b9217
Lucio Flavio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2019-02-11 14:58:53 -0300

    added desktop package

---
+++ standalone/desktop/src/Boot.java
@@ -0,0 +1,18 @@
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+
+public class Boot {
+
+  public static void main(String[] args) {
+    System.out.println("Boot > main");
+    
+    Profile profile = new ProfileImpl();
+    profile.setParameter(Profile.PLATFORM_ID, "HardwareSnifferPlatform");
+    profile.setParameter(Profile.CONTAINER_NAME, "TestContainer");
+    profile.setParameter(Profile.MAIN_HOST, "localhost");
+    profile.setParameter(Profile.GUI, Boolean.TRUE.toString());
+
+    Runtime.instance().createMainContainer(profile);
+  }
+}
